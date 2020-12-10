8e5dcc52832640bec76f611eeb3b49297a869fc0
kotarofunyu <kotarofunyu@gmail.com>
2020-12-10 18:38:03 +0900

    [FIX] comment out binding.pry

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,7 +2,7 @@ class LinebotController < ApplicationController
   require 'line/bot'
 
   def callback
-    binding.pry
+    # binding.pry
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
