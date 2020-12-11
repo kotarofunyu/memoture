815cc05739eb6a3e255705a64bbaba1463486e29
kotarofunyu <kotarofunyu@gmail.com>
2020-12-11 13:54:23 +0900

    [typo]

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,7 +1,7 @@
 class LinebotController < ApplicationController
   require 'line/bot'
   @client = Line::Bot::Client.new do |config|
-    config.channel_access_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
+    config.channel_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
   end
 
