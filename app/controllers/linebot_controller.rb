eb11784b884209a6621b6312e26fcbf50fe6eba0
kotarofunyu <kotarofunyu@gmail.com>
2022-06-12 00:57:46 +0900

    ビジネスロジックをモデルに切り出した

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,57 +1,11 @@
 class LinebotController < ApplicationController
-  require 'line/bot'
-  def client
-    @client = Line::Bot::Client.new do |config|
-      config.channel_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    end
-  end
-
   def callback
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    @registration = Memos::Registration.new(body: request.body.read, signature: request.env['HTTP_X_LINE_SIGNATURE'])
 
-    if client.validate_signature(body, signature)
-      events = client.parse_events_from(body)
-      event = events.select { _1.class == Line::Bot::Event::Message }.first
-      if event.type == Line::Bot::Event::MessageType::Text
-        text = event.message['text']
-        first_line = text.split("\n")[0]
-        message = { type: 'text', text: "メモしました\n---------------\n#{create(text)}" }
-        client.reply_message(event['replyToken'], message)  
-        head :ok
-      else
-        head :unprocessable_entity
-      end
+    if @registration.save
+      head :ok
     else
-      head :bad_request
+      head :unprocessable_entity
     end
-  end
-
-  private
-
-  def create(text)
-    if text.include?("twitter.com")
-      tweet = Tweet.get_tweet_object(text)
-      memo = Memo.new(text: tweet.text)
-      memo.tweet = Tweet.new(
-        url: text,
-        user_name: tweet.user.name,
-        user_screen_name: tweet.user.screen_name
-      )
-      memo.save!
-    else
-      memo = Memo.create(text: text)
-    end
-    return memo.text
-  end
-
-  def twitter_save(text)
-    if text.include?("?")
-      tweet_id = text.split('?')[0].split('/').grep(/^[0-9]+$/)[0].to_i
-    else
-      tweet_id = text.split('/').grep(/^[0-9]+$/)[0].to_i
-    end
-    return tweet_content = Tweet.get_tweet_full_text(tweet_id)
   end
 end
---
+++ app/models/memos/registration.rb
@@ -0,0 +1,65 @@
+require 'line/bot'
+
+class Memos::Registration
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  define_model_callbacks :save
+
+  attribute :body
+  attribute :signature
+
+  with_options presence: true do
+    validates :body
+    validates :signature
+  end
+
+  validate do
+    errors.add(:base, :invalid) unless client.validate_signature(body, signature)
+  end
+
+  validate do
+    errors.add(:base, :invalid) unless event.type == Line::Bot::Event::MessageType::Text
+  end
+
+  before_save { throw(:abort) if invalid? }
+
+  after_save do
+    message = { type: 'text', text: "メモしました\n---------------\n#{text}" }
+    client.reply_message(event['replyToken'], message)  
+  end
+
+  def save
+    run_callbacks :save do
+      if text.include?("twitter.com")
+        tweet = Tweet.get_tweet_object(text)
+        memo = Memo.new(text: tweet.text)
+        memo.build_tweet(
+          url: text,
+          user_name: tweet.user.name,
+          user_screen_name: tweet.user.screen_name
+        )
+        memo.save!
+      else
+        Memo.create!(text: text)
+      end
+    end
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_token = ENV["LINE_CHANNEL_ACCESS_TOKEN"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    end
+  end
+
+  def event
+    @event ||= client.parse_events_from(body).select { _1.class == Line::Bot::Event::Message }.first
+  end
+
+  def text
+    event.message['text']
+  end
+end
